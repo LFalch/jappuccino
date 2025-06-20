d025159134c2278857aba3db2350c16071d24eef
LFalch <lucas@wasd.dk>
2025-06-20 15:40:05 +0200

    Remove panics in modified_utf8
    Something has gone very wrong if this module panicks now

--- src/modified_utf8.rs
+++ src/modified_utf8.rs
@@ -7,6 +7,7 @@ pub fn read_modified_utf8<R: Read>(mut reader: R, length: usize) -> io::Result<S
     let error = || {
         io::Error::new(
             io::ErrorKind::InvalidData,
+            // TODO: perhaps give more specific errors in a custom type
             "incorrect \"modified\" utf-8 byte",
         )
     };
@@ -19,14 +20,14 @@ pub fn read_modified_utf8<R: Read>(mut reader: R, length: usize) -> io::Result<S
             1..=0x7f => string.push(b as char),
             x @ 0b1100_0000..=0b1101_1111 => {
                 let x = x as u32;
-                let y = bytes.next().unwrap()? as u32;
+                let y = bytes.next().ok_or_else(error)?? as u32;
                 let codepoint = (((x) & 0x1f) << 6) + ((y) & 0x3f);
                 string.push(char::from_u32(codepoint).ok_or_else(error)?);
             }
             x @ 0b1110_0000..=0b1110_1111 => {
                 let x = x as u32;
-                let y = bytes.next().unwrap()? as u32;
-                let z = bytes.next().unwrap()? as u32;
+                let y = bytes.next().ok_or_else(error)?? as u32;
+                let z = bytes.next().ok_or_else(error)?? as u32;
                 let codepoint = ((x & 0xf) << 12) + ((y & 0x3f) << 6) + (z & 0x3f);
                 if let Some(c) = char::from_u32(codepoint) {
                     string.push(c);
